61c8bd343851251af95f1052f0a32026c4e4359e
Jose Garrido <a24galujo@iesgrancapitan.org>
2026-01-17 19:33:41 +0100

    db schema created

---
+++ src/main/java/org/main/InitDataBase.java
@@ -0,0 +1,26 @@
+package org.main;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InitDataBase {
+    public static void main(String[] args) throws Exception {
+
+        //Creo la conexion
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:src/data/info.db");
+        Statement stmt = conn.createStatement();
+
+        System.out.println("Conectado a SQLite correctamente");
+
+        // Leer todó el contenido de schema.sql
+        String sql = Files.readString(Paths.get("src/main/resources/schemas.sql"));
+        stmt.executeUpdate(sql);
+
+        stmt.close();
+        conn.close();
+
+    }
+}
